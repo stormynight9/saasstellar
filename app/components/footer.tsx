6dbcf5307d8f605e38a6199ff7efd63db04b4d34
stormynight9 <ferjani.nader@hotmail.fr>
2023-09-11 14:14:31 +0100

    add: my handle in footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,16 @@
 const Footer = () => {
     return (
-        <footer className='mt-16 flex h-32 items-end bg-gradient-to-b from-transparent to-primary/20'>
+        <footer className='mt-16 flex h-32 flex-col items-center justify-between bg-gradient-to-b from-transparent to-primary/20'>
+            <div className='p-5 text-center'>
+                Looking for a frontend developer with React/Astro skills? Reach
+                out to me at{' '}
+                <a
+                    href='mailto:ferjani.nader@hotmail.fr'
+                    className='whitespace-nowrap font-medium text-orange-400'
+                >
+                    ferjani.nader@hotmail.fr 🚀
+                </a>{' '}
+            </div>
             <div className='mt-auto h-[1px] w-full bg-gradient-to-r from-transparent via-primary to-transparent'></div>
         </footer>
     )
