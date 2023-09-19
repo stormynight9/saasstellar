6a49c5fcb244aaf72b8a0cdc698795a44e03fd44
stormynight9 <ferjani.nader@hotmail.fr>
2023-09-19 07:31:36 +0100

    fix: banner logo

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -4,8 +4,12 @@ import { Label } from './ui/label'
 import saasstellar from '../assets/saasstellar.svg'
 import { useForm } from '@formspree/react'
 import { Loader2Icon } from 'lucide-react'
+import Saastellar from './icons/saasstellar'
+import { cn } from '~/lib/utils'
+import useTheme from '~/hooks/use-theme'
 const Banner = () => {
     const [state, handleSubmit] = useForm('mjvqrzpz')
+    const [theme, setTheme] = useTheme()
     return (
         <section className=' relative mt-40 flex flex-col items-center justify-between gap-10 bg-gradient-to-br from-primary/20 via-transparent to-primary/20'>
             <div className='h-[1px] w-full bg-gradient-to-r from-primary to-transparent'></div>
@@ -81,13 +85,31 @@ const Banner = () => {
                             )}
                         </form>
                     </div>
-                    <img
+                    <Saastellar
+                        linearFrom='text-primary'
+                        className='absolute -bottom-10 right-5 hidden w-48 lg:block'
+                        linearTo={cn(
+                            'text-primary',
+                            theme === 'zinc' && 'text-zinc-600',
+                            theme === 'slate' && 'text-slate-600',
+                            theme === 'stone' && 'text-stone-600',
+                            theme === 'gray' && 'text-gray-600',
+                            theme === 'neutral' && 'text-neutral-600',
+                            theme === 'red' && 'text-red-600',
+                            theme === 'rose' && 'text-pink-600',
+                            theme === 'orange' && 'text-rose-600',
+                            theme === 'green' && 'text-emerald-600',
+                            theme === 'blue' && 'text-purple-600',
+                            theme === 'yellow' && 'text-yellow-600',
+                            theme === 'violet' && 'text-violet-600'
+                        )}
+                    />
+                    {/* <img
                         src={saasstellar}
                         alt=''
                         width={196}
-                        height={194}
                         className='absolute -bottom-10 right-5 hidden lg:block'
-                    />
+                    /> */}
                 </div>
             </div>
             <div className='h-[1px] w-full bg-gradient-to-l from-primary to-transparent'></div>
